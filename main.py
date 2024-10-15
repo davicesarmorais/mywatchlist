233148e10ce5f51fab20a2be3513e1c19103a673
Davi César <davicesarmorais@gmail.com>
2024-10-15 16:47:48 -0300

    feat/integracao com banco de dados

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template, request,redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
-with open ('tests/teste.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)["data"]
+def data_by_user(user_id: int) -> list:
+    with sqlite3.connect('data.db') as con:
+        cur = con.cursor()
+        cur.execute(
+            f"""SELECT title_name, type, genre, watched
+            FROM title_by_user
+            JOIN title ON title_by_user.title_id = title.id
+            WHERE user_id = {user_id}"""
+        )
+        data = cur.fetchall()
+    
+    dados = [
+        {"titulo": item[0],
+        "tipo": item[1],
+        "genero": [x for x in item[2].split(",")],
+        "status": "Assistido" if item[3] == 1 else "Não assistido"} 
+        for item in data
+    ]
+
+    return dados
+    
 
 @app.route('/')
 def home():
+    dados = data_by_user(1)
+    
     status = request.args.get('status', "Todos")
     if status == 'Todos':
-        dados_filtrados = data
+        dados_filtrados = dados
     else:
-        dados_filtrados = [item for item in data if item['status'] == status]
+        dados_filtrados = [item for item in dados if item['status'] == status]
     
-    return render_template("ideia.html", data=dados_filtrados, status=status)
     return render_template("home.html", data=dados_filtrados, status=status)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form['novo_titulo']
+    with sqlite3.connect('data.db') as con:
+        cur = con.cursor()  
+        cur.execute(
+            f"""INSERT INTO title_by_user (user_id, title_id, title_order, watched)
+            VALUES (1, (SELECT id FROM title WHERE title_name = '{title}'), 7, 1)"""
+        )
+        con.commit()
+
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
--- managedb.py
+++ managedb.py
@@ -1,9 +1,16 @@
-# import sqlite3
+import sqlite3
 
-# con = sqlite3.connect("data.db")
-# print("Opened database successfully")
+con = sqlite3.connect("data.db")
+print("Opened database successfully")
 
-# con.execute("CREATE TABLE user(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, password TEXT)")
+con.execute("CREATE TABLE IF NOT EXISTS title(id INTEGER PRIMARY KEY AUTOINCREMENT, title_name TEXT, type TEXT, genre TEXT)")
+con.execute("CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, password TEXT)")
+con.execute("CREATE TABLE IF NOT EXISTS title_by_user(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, title_id INTEGER, watched BOOLEAN, title_order INTEGER, FOREIGN KEY(user_id) REFERENCES user(id), FOREIGN KEY(title_id) REFERENCES title(id))")
 
-# con.execute("CREATE TABLE title_by_user(id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, title_id INTEGER)")
+con.execute("INSERT INTO user(name, password) VALUES (?,?)", ("davi", "123"))
+con.execute("INSERT INTO title(title_name, type, genre) VALUES (?,?,?)", ("The Godfather", "Filme", "Drama"))
+con.execute("INSERT INTO title(title_name, type, genre) VALUES (?,?,?)", ("Inception", "Filme", "Ação"))
+con.execute("INSERT INTO title(title_name, type, genre) VALUES (?,?,?)", ("Dexter", "Serie", "Crime"))
+con.execute("INSERT INTO title(title_name, type, genre) VALUES (?,?,?)", ("Breaking Bad", "Serie", "Crime"))
+con.commit()
 
